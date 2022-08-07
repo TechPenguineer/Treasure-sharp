9083346c9d22134ad57f7495c37991b49545d387
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2022-08-07 15:30:04 -0400

    basic

---
+++ LIB/treaure.cs
@@ -0,0 +1,21 @@
+﻿
+namespace treasure.privacy
+{
+    public class appKey
+    {
+       public void createPopup(string codeFormat)
+        {
+            string message = "Please use your activation key to continue.";
+            string title = "Activation";
+            MessageBoxButtons buttons = MessageBoxButtons.OKCancel;
+            MessageBox.Show(message, title, buttons);
+        }
+
+
+        public static void main(string[] args)
+        {
+            appKey appKey = new appKey();
+            appKey.createPopup("xxxx-xxxx-xxxx");
+        }
+    }
+}
